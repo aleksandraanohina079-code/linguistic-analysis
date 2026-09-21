2707ad39e4a5b17f0a85f3e28339b664bb8c9739
aleksandraanohina079-code <aleksandraanohina079@gmail.com>
2026-09-21 10:38:58 +0300

    Add basic text analyzer

---
+++ analyzer.py
@@ -0,0 +1,9 @@
+text = """
+Language is a system of signs used for communication.
+Linguistics studies language and its structure.
+Computational linguistics combines linguistics and computer science.
+"""
+
+words = text.split()
+
+print("Number of words:", len(words))
